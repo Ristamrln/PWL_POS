8de80a286c35bdc09defd371ede743b7d25eac80
RistaMarlinaHutagaol <ristamarlina2604@gmail.com>
2024-03-14 08:22:03 +0700

    Praktikum-1 JS3

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,12 +12,13 @@ class UserController extends Controller
     {   
         // tambah data user dengan Eloquent Model
         $data = [
-            'username' => 'customer-1',
-            'nama' => 'Pelanggan',
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
             'password' => Hash::make('12345'),
-            'level_id' => 3
+            
         ];
-        UserModel::insert($data); //tambahkan data ke tabel m_user
+        UserModel::create($data);
         
         //coba akses model UserModel
         $user = UserModel::all(); //ambil semua data dari m_user
