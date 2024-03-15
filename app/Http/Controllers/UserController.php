1ec3ad8a46222e4ae8c3576c9002a5ca61647dcc
RistaMarlinaHutagaol <ristamarlina2604@gmail.com>
2024-03-15 11:16:25 +0700

    pwl 4 Praktikum-2.5

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -21,18 +21,41 @@ class UserController extends Controller
         // UserModel::create($data);
         
         //coba akses model UserModel
-        $user = UserModel::firstOrNew(
-            [
-                'username' => 'manager33',
-                'nama' => 'Manager Tiga Tiga',
+        $user = UserModel::create([
+                'username' => 'manager11',
+                'nama' => 'Manager11',
                 'password'=> Hash::make('12345'),
                 'level_id' => 2
             
             ],
         ); 
+
+        $user->username = 'manager12';
+
+        // $user->isDirty(); //true
+        // $user->isDirty('username'); //true
+        // $user->isDirty('nama'); //false
+        // $user->isDirty(['nama', 'username']); //true
+
+        // $user->isClean(); //false
+        // $user->isClean('username'); //false
+        // $user->isClean('nama'); //true
+        // $user->isClean(['nama', 'username']); //false
+
         $user->save();
+
+        $user->wasChanged(); // true
+        $user->wasChanged('username'); //true
+        $user->wasChanged(['username', 'level_id']); //true
+        $user->wasChanged('nama');//false
+        dd($user->wasChanged(['nama', 'username']));//true
+
+        // $user->isDirty(); //false
+        // $user->isClean(); //true 
+        // dd($user->isDirty());
         //ambil semua data dari m_user
         // dd($user);
-        return view('user', ['data' => $user]);
+
+        // return view('user', ['data' => $user]);
 }
 }
