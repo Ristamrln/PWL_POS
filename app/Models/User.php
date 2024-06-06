f871012f5f7643aced071b21a36879683ae5ca5e
RistaMarlinaHutagaol <ristamarlina2604@gmail.com>
2024-06-06 22:44:39 +0700

    JS 10 Praktikum 2

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,16 +3,37 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Foundation\Auth\User as UserAuthenticate;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
 /**
  * @mixin IdeHelperUser
  */
-class User extends Model
+class User extends UserAuthenticate implements JWTSubject
 {
     use HasFactory;
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier(): mixed
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims(): array
+    {
+        return [];
+    }
 
     protected $table = 'm_user';
     public $timestamps = false;
